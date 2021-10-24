658ad85e3d79224fb500c9126a79fc3bc8dbb1d1
Vinicius Mitsuo Carvalho Carrion Moises <37308081+vinicarrion@users.noreply.github.com>
2021-10-24 15:49:47 -0300

    Add files via upload

---
+++ modelo-fisico ValorantFinder.sql
@@ -0,0 +1,21 @@
+create database ValorantFinder
+use ValorantFinder
+
+create table Jogadores(
+id integer auto_increment not null,
+usuario varchar(70) not null,
+senha varchar(20) not null,
+email varchar(45) not null,
+apelido varchar(30) not null,
+contato varchar(50) not null,
+primary key (id) 
+);
+
+create table Times(
+codigo integer auto_increment not null,
+nome varchar(90) not null,
+siglas varchar(5) not null,
+id_jogador integer not null,
+primary key(codigo),
+foreign key (id_jogador) references Jogadores(id)
+);
