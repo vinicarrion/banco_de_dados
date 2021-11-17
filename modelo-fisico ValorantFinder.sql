269d6235dd2c0bd9b63faf01ccf4b2de610c8ef1
Vinicius Mitsuo Carvalho Carrion Moises <37308081+vinicarrion@users.noreply.github.com>
2021-11-16 21:40:01 -0300

    Update modelo-fisico ValorantFinder.sql

--- modelo-fisico ValorantFinder.sql
+++ modelo-fisico ValorantFinder.sql
@@ -3,19 +3,30 @@ use ValorantFinder
 
 create table Jogadores(
 id integer auto_increment not null,
-usuario varchar(70) not null,
-senha varchar(100) not null,
-email varchar(45) not null,
-apelido varchar(30) not null,
+usuario varchar(11) not null,
+email varchar(128) not null,
+senha varchar(128) not null,
 contato varchar(50) not null,
+apelido varchar(30) not null,
+bio varchar(250),
+imagem varchar(200),
 primary key (id) 
 );
 
 create table Times(
-codigo integer auto_increment not null,
-nome varchar(90) not null,
+id integer auto_increment not null,
+nome varchar(25) not null,
 siglas varchar(5) not null,
+data_criacao date not null,
+logo_time varchar(200),
+primary key(id)
+);
+
+create table times_jogadores(
 id_jogador integer not null,
-primary key(codigo),
-foreign key (id_jogador) references Jogadores(id)
+id_time integer,
+administrador tinyint not null,
+primary key (id_jogador),
+foreign key (id_jogador) references jogadores(id),
+foreign key (id_time) references times(id)
 );
